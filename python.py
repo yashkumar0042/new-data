036cd1da0ac331803901178d2f79e829af1bd958
yashkumar0042 <yash.kumar0042@gmail.com>
2024-03-01 08:46:00 +0530

    removed special characters

--- python.py
+++ python.py
@@ -1,1 +1,1 @@
-print("hello welcome toPython [D[D[D[D[D[D[D[D[C [F[B[F[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[6~p[2~ython"
+print("hello welcome to Python")
